90219f4675986fc291f02cafcc0d37e856fec5f4
ArtemZayarny <artyom.zayarny@gmail.com>
2023-11-11 20:06:31 +0200

    added usefull libs

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
-import express from 'express';
+import express, { Express } from 'express';
+import helmet from 'helmet';
+// @ts-ignore
+import xss from 'xss-clean';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import morgan from 'morgan';
 import { todoRouter } from './routes/todoRoutes.ts';
 
-const app = express();
+const app: Express = express();
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!');
-// });
-// Body parser, reading data from body
+// set security HTTP headers
+app.use(helmet());
+
+// Development logging http request
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Parse json request body
 app.use(express.json());
+
+// sanitize request data
+app.use(xss());
+app.use(ExpressMongoSanitize());
+
 // Mountaining routing
 app.use('/api/v1/todos', todoRouter);
 
