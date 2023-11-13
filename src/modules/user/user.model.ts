27993a824db98d28d205a2dbf8feb3962059fdc9
ArtemZayarny <artyom.zayarny@gmail.com>
2023-11-13 11:43:39 +0200

    added user model

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,1 +1,38 @@
-//User model
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide valid email'],
+  },
+  photo: { type: String, default: 'default.jpeg' },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8,
+    select: false,
+    validate(value: string) {
+      if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+        throw new Error(
+          'Password must contain at least one letter and one number',
+        );
+      }
+    },
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
