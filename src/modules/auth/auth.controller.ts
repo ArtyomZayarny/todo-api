1f3884b7fe6e68a62be78719038b74ae257cdd53
ArtemZayarny <artyom.zayarny@gmail.com>
2023-11-14 22:16:36 +0200

    added auth handlers

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,1 +1,16 @@
-export const authController = () => {};
+import { NextFunction, Request, Response } from 'express';
+import { catchAsync } from '../../utils/catchAsync.ts';
+import * as userService from '../user/user.service.ts';
+import httpStatus from 'http-status';
+
+export const register = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    //register user
+    const user = await userService.createUser(req.body);
+    //generate token
+    //send user to client
+    res.status(httpStatus.CREATED).send({ user });
+  },
+);
+
+export const login = catchAsync(async (req: Request, res: Response) => {});
