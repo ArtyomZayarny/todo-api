1ee235fcfa071b8fc4f60823260a2521e883ec6d
ArtemZayarny <artyom.zayarny@gmail.com>
2023-11-15 22:15:09 +0200

    register new user with token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,14 +2,16 @@ import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../../utils/catchAsync.ts';
 import * as userService from '../user/user.service.ts';
 import httpStatus from 'http-status';
+import { tokenService } from '../token/index.ts';
 
 export const register = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     //register user
     const user = await userService.createUser(req.body);
     //generate token
+    const token = await tokenService.generateToken(user);
     //send user to client
-    res.status(httpStatus.CREATED).send({ user });
+    res.status(httpStatus.CREATED).send({ user, token });
   },
 );
 
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,13 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import * as userService from './user.service.ts';
 import { catchAsync } from '../../utils/catchAsync.ts';
 import { User } from './user.model.ts';
 
-export const createUser = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const user = await User.create(req.body);
-    res.status(httpStatus.CREATED).send(user);
-  },
-);
+export const createUser = catchAsync(async (req: Request, res: Response) => {
+  const user = await User.create(req.body);
+  res.status(httpStatus.CREATED).send(user);
+});
 
 export const getUsers = catchAsync(async (req: Request, res: Response) => {
   const users = await userService.getAllUsers();
