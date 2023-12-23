3206580d90d0402b8fce22e60e6fee3768ffd1b1
ArtemZayarny <artyom.zayarny@gmail.com>
2023-12-23 20:59:53 +0200

    Added template for email confirmation

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -1,4 +1,6 @@
 import { injectable } from 'inversify';
+import * as ejs from 'ejs';
+import * as path from 'path';
 import config from '../../config/config.ts';
 import nodemailer, { TransportOptions } from 'nodemailer';
 
@@ -8,17 +10,11 @@ export const transport = nodemailer.createTransport(
 
 @injectable()
 export class EmailService {
-  public async sendEmail(
-    to: string,
-    subject: string,
-    text: string,
-    html: string,
-  ) {
+  public async sendEmail(to: string, subject: string, html: string) {
     const msg = {
       from: config.email.from,
       to,
       subject,
-      text,
       html,
     };
     await transport.sendMail(msg);
@@ -27,13 +23,16 @@ export class EmailService {
   public async sendVerificationEmail(to: string, token: string, name: string) {
     const subject = 'Email Verification';
     // replace this url with the link to the email verification page of your front-end app
-    const verificationEmailUrl = `http://${config.clientUrl}/verify-email?token=${token}`;
-    const text = `Hi ${name},
-    To verify your email, click on this link: ${verificationEmailUrl}
-    If you did not create an account, then ignore this email.`;
-    const html = `<div style="margin:30px; padding:30px; border:1px solid black; border-radius: 20px 10px;"><h4><strong>Hi ${name},</strong></h4>
-    <p>To verify your email, click on this link: ${verificationEmailUrl}</p>
-    <p>If you did not create an account, then ignore this email.</p></div>`;
-    await this.sendEmail(to, subject, text, html);
+    const verificationEmailUrl = `http://${config.clientUrl}/api/v1/auth/verify-email?token=${token}`;
+
+    const html = await ejs.renderFile(
+      path.resolve(__dirname, '../../../src/views/email-verification.ejs'),
+      {
+        name,
+        verificationEmailUrl,
+      },
+    );
+
+    await this.sendEmail(to, subject, html);
   }
 }
